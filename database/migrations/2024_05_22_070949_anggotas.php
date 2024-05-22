2ece7d839b3af5a903107e4aa8dc502683038405
MDanilAkbar <mdanilakbar.005@gmail.com>
2024-05-22 14:18:39 +0700

    membuat migration (tabel)

--- database/migrations/2024_05_22_070949_anggotas.php
+++ database/migrations/2024_05_22_070949_anggotas.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('anggotas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->string('nisn',50)->unique();
+            $table->string('nama',100);
+            $table->string('tempatLahir',100);
+            $table->date('tanggalLahir');
+            $table->string('jenisKelamin',20);
+            $table->string('agama',20);
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
